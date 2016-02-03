272745ec21549a86155d59e792074ae0af51cfa8
Pawel Ropa <pawel.ropa@gmail.com>
2016-02-03 10:25:04 +0100

    Added comments to hello.go file

--- chapter1/hello/hello.go
+++ chapter1/hello/hello.go
@@ -1,10 +1,10 @@
 // Copyright © 2010-12 Qtrac Ltd.
-// 
+//
 // This program or package and any associated files are licensed under the
 // Apache License, Version 2.0 (the "License"); you may not use these files
 // except in compliance with the License. You can get a copy of the License
 // at: http://www.apache.org/licenses/LICENSE-2.0.
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,6 +14,7 @@
 // hello.go
 package main
 
+// Importing three packages from standard library
 import (
     "fmt"
     "os"
@@ -21,9 +22,9 @@ import (
 )
 
 func main() {
-    who := "World!"
+    who := "World!" // Short variable declaration
     if len(os.Args) > 1 { /* os.Args[0] is "hello" or "hello.exe" */
-        who = strings.Join(os.Args[1:], " ")
+        who = strings.Join(os.Args[1:], " ") // Take slice of arguments passed to program and join them using space character
     }
     fmt.Println("Hello", who)
 }
